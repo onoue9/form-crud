8eba806c43d6c3a9fb4d45b888e18926d3a16da7
Filipe Onoue <filpeflom@gmail.com>
2023-05-16 08:49:57 -0300

    refactor: :recycle: refact the path to import globals.css

--- src/pages/[id]/update-user.tsx
+++ src/pages/[id]/update-user.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation";
 import { useRouter } from "next/router";
 import { useState } from "react";
-//import '../app/globals.css';
+import '../../app/globals.css';
 
 export default function UpdateUser() {
 
